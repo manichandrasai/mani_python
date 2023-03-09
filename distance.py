e6f29350e2e0d7982e575ebf04a10db8faf70456
manichandrasai <manichandrasaimani@gmail.com>
2023-03-09 19:22:43 +0530

    distance

---
+++ distance.py
@@ -0,0 +1,6 @@
+x1 = int(input("enter a num:"))
+x2 = int(input("enter a num:"))
+y1 = int(input("enter a num:"))
+y2 = int(input("enter a num:"))
+distance = ((x2-x1)**2 +(y2-y1)**2)**(1/2)
+print(distance)
