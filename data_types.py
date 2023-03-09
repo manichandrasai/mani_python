df36ce86d89fa3c29b20c8aeba564b5deebc430f
manichandrasai <manichandrasaimani@gmail.com>
2023-03-09 19:21:29 +0530

    data types

---
+++ data_types.py
@@ -0,0 +1,22 @@
+y = 42
+z = 32
+x = 45
+print(x)
+print(type(y))
+print(type(z))
+print(z)
+
+a = 23.5
+b = 45.5
+print(a)
+print(type(b))
+print(a,b)
+
+name = "mani"
+print(name)
+print(type(name))
+
+t = True
+print(t)
+print(type(t))
+print(t,x)
