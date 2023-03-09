08b3cc52c0e7863fb3be461c1fa648f50d7490b4
manichandrasai <manichandrasaimani@gmail.com>
2023-03-09 19:15:19 +0530

    area and perimeter of circle and square

---
+++ area_permimeter.py
@@ -0,0 +1,15 @@
+num = int(input("enter a num:"))
+area = 3.14*num*num
+print(area)
+
+num = int(input("enter a num:"))
+p = 2*3.14*num
+print(p)
+
+num = int(input("enter a num:"))
+p = 4*num
+print(p)
+
+num = int(input("enter a num:"))
+a = num*num
+print(a)
