4ec6696b8ab33e064d0aeb993a0d70ebd7e8d1b6
manichandrasai <manichandrasaimani@gmail.com>
2023-03-09 19:16:48 +0530

    area of triangle

---
+++ area_triangle.py
@@ -0,0 +1,6 @@
+s = int(input("enter the side:"))
+a = int(input("enter the a value:"))
+b = int(input("enter the b value:"))
+c = int(input("enter the c value:"))
+m = (s*(s-a)*(s-b)*(s-c))**0.5
+print(m)
