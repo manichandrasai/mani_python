8003f142d24947a0aab5b6b4098e93e3ae2a7674
manichandrasai <manichandrasaimani@gmail.com>
2023-04-09 12:44:46 +0530

    assignment

--- m.py
+++ m.py
@@ -1,4 +1,10 @@
-f = open("data.csv","r")
-s = f.read()
-f.close()
-print(s)
+# f = open("data.csv","r")
+# s = f.read()
+# f.close()
+# # print(s)
+n = int(input("enter a num:"))
+for i in range(2,n):
+    if n % i == 0:
+        print("not prime")
+else:
+    print("prime")    
--- welcome.py
+++ welcome.py
@@ -1,1 +1,8 @@
-print('hloo mani')
+# print('hloo mani')
+def sum_avg(x, y, z):
+    s = x + y + z
+    a = s/3.0
+    return s, a # function returning multiple values
+
+# total, avg = sum_avg(3, 4 ,5)
+print(sum_avg(2,3,4))
